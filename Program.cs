0af39cbcf4eb7c1d0806b37ab202bc2212ab0049
Brandt Redd <bredd@users.noreply.github.com>
2020-03-13 13:00:09 -0600

    Add open source license and ReadMe.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,36 @@
-﻿using System;
+﻿/*
+BSD 3-Clause License
+
+Copyright (c) 2020, Brandt Redd
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +39,6 @@ using System.IO;
 using System.Globalization;
 using System.Diagnostics;
 using FileMeta;
-
 
 namespace ReadAndConvertCovid19Data
 {
@@ -29,7 +60,7 @@ https://github.com/FileMeta/ReadAndConvertCovid19Data
         const string c_covid19ConfirmedUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
         const string c_covid19DeathsUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv";
         const string c_covid19RecoveredUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv";
-        const string c_covid19OutputFilename = "COVID-19-Date-Series-csse.csv";
+        const string c_covid19OutputFilename = "COVID-19-Time-Series-csse.csv";
 
         // UTF8 encoding with now byte-order mark.
         static Encoding s_UTF8_No_BOM = new UTF8Encoding(false, true);
@@ -53,6 +84,7 @@ https://github.com/FileMeta/ReadAndConvertCovid19Data
                 // Generate filename
                 string outPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), c_covid19OutputFilename);
 
+                Console.WriteLine();
                 Console.WriteLine($"Writing combined time series data to '{outPath}'.");
                 WriteData(dataSet, outPath);
 
