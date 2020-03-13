3e5c808fb85383b07f751912059bda139934f51b
Brandt Redd <bredd@users.noreply.github.com>
2020-03-13 11:36:12 -0600

    Add project files.

---
+++ CsvReader.cs
@@ -0,0 +1,193 @@
+﻿/*
+---
+name: CsvReader.cs
+description: CodeBit classes for reading data from .csv format according to rfc4180
+url: https://raw.githubusercontent.com/FileMeta/CsvReader/master/CsvReader.cs
+version: 1.0
+keywords: CodeBit
+dateModified: 2020=03-12
+license: https://opensource.org/licenses/BSD-3-Clause
+# Metadata in MicroYaml format. See http://filemeta.org/CodeBit.html
+...
+*/
+
+/*
+=== BSD 3 Clause License ===
+https://opensource.org/licenses/BSD-3-Clause
+
+Copyright 2020 Brandt Redd
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FileMeta
+{
+    /// <summary>
+    /// Read the contents of a .csv file according to rfc4180 specifications.
+    /// </summary>
+    class CsvReader : IDisposable
+    {
+        TextReader m_reader;
+        bool m_autoCloseReader;
+
+        /// <summary>
+        /// Construct a .csv reader on a file in the file system
+        /// </summary>
+        /// <param name="filename">Name of the file</param>
+        /// <param name="encoding">Expected text encoding in the file. Default: UTF8</param>
+        /// <param name="detectEncodingFromByteOrderMarks">True if reader should look for
+        /// byte-order marks that indicate encoding. Default: true</param>
+        /// </remarks>
+        public CsvReader(string filename, Encoding encoding = null, bool detectEncodingFromByteOrderMarks = true)
+        {
+            if (encoding == null) encoding = Encoding.UTF8;
+            m_reader = new StreamReader(filename, Encoding.UTF8, detectEncodingFromByteOrderMarks);
+            m_autoCloseReader = true;
+        }
+
+        /// <summary>
+        /// Construct a .csv reader from a <see cref="TextReader"/>
+        /// </summary>
+        /// <param name="reader">The reader containing the source .csv file.</param>
+        /// <param name="autoCloseReader">True if the <see cref="TextReader"/> should be closed when the CsvReader is disposed.</param>
+        public CsvReader(TextReader reader, bool autoCloseReader = true)
+        {
+            m_reader = reader;
+            m_autoCloseReader = autoCloseReader;
+        }
+
+        /// <summary>
+        /// Read one line from a CSV file
+        /// </summary>
+        /// <returns>An array of strings parsed from the line or <b>null</b> if at end-of-file.</returns>
+        /// <remarks>A well-formed .csv file will have the same number of elements in each row (line).
+        /// However, that's not guaranteed to be the case. In the event that a .csv file has a variable
+        /// number of elements in each row - or if quoting and escaping is not consistent with rfc4180
+        /// then lines may have different numbers of elements.
+        /// </remarks>
+        public string[] Read()
+        {
+            if (m_reader.Peek() < 0) return null;
+
+            List<string> line = new List<string>();
+            StringBuilder builder = new StringBuilder();
+
+            for (; ; )
+            {
+                int c = m_reader.Read();
+                char ch = (c >= 0) ? (char)c : '\n'; // Treat EOF like newline.
+
+                // Reduce CRLF to LF
+                if (ch == '\r')
+                {
+                    if (m_reader.Peek() == '\n') continue;
+                    ch = '\n';
+                }
+
+                if (ch == '\n')
+                {
+                    line.Add(builder.ToString());
+                    break;
+                }
+                else if (ch == ',')
+                {
+                    line.Add(builder.ToString());
+                    builder.Clear();
+                }
+                else if (ch == '"')
+                {
+                    for (; ; )
+                    {
+                        c = m_reader.Read();
+                        if (c < 0) break;
+                        ch = (char)c;
+
+                        if (ch == '"')
+                        {
+                            if (m_reader.Peek() == (int)'"')
+                            {
+                                // Double quote means embedded quote
+                                m_reader.Read(); // read the second quote
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        builder.Append(ch);
+                    }
+                } // if quote
+                else
+                {
+                    builder.Append(ch);
+                }
+            } // Loop exits in the middle
+
+            return line.ToArray();
+        }
+
+        #region IDisposable Support
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (m_reader != null)
+            {
+                if (m_autoCloseReader)
+                {
+                    m_reader.Dispose();
+                }
+                m_reader = null;
+#if DEBUG
+                if (!disposing)
+                {
+                    System.Diagnostics.Debug.Fail("Failed to dispose CsvReader.");
+                }
+#endif
+            }
+        }
+
+        ~CsvReader()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(false);
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Globalization;
+using System.Diagnostics;
+using FileMeta;
+
+
+namespace ReadAndConvertCovid19Data
+{
+
+    using DataSet = Dictionary<DateTime, Dictionary<DataKey, DataRecord>>;
+
+    class Program
+    {
+        const string c_message =
+@"Source is the the current COVID-19 data posted by Johns Hopkins University
+on GitHub at https://github.com/CSSEGISandData/COVID-19
+
+Data are reorganized into a .csv file suitable for analysis in Microsoft
+Excel or Tableau or other analytics tools. For more information, see the
+open source project at
+https://github.com/FileMeta/ReadAndConvertCovid19Data
+";
+
+        const string c_covid19ConfirmedUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+        const string c_covid19DeathsUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv";
+        const string c_covid19RecoveredUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv";
+        const string c_covid19OutputFilename = "COVID-19-Date-Series-csse.csv";
+
+        // UTF8 encoding with now byte-order mark.
+        static Encoding s_UTF8_No_BOM = new UTF8Encoding(false, true);
+
+        enum DataType { Confirmed, Deaths, Recovered };
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(c_message);
+
+                var dataSet = new DataSet();
+                Console.WriteLine("Reading from " + c_covid19ConfirmedUrl);
+                ReadData(c_covid19ConfirmedUrl, dataSet, DataType.Confirmed);
+                Console.WriteLine("Reading from " + c_covid19DeathsUrl);
+                ReadData(c_covid19DeathsUrl, dataSet, DataType.Deaths);
+                Console.WriteLine("Reading from " + c_covid19RecoveredUrl);
+                ReadData(c_covid19RecoveredUrl, dataSet, DataType.Recovered);
+
+                // Generate filename
+                string outPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), c_covid19OutputFilename);
+
+                Console.WriteLine($"Writing combined time series data to '{outPath}'.");
+                WriteData(dataSet, outPath);
+
+                Console.WriteLine($"Done.");
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.ToString());
+            }
+
+            Win32Interop.ConsoleHelper.PromptAndWaitIfSoleConsole();
+        }
+
+        // Even with this cache policy, the underlying system still caches for up to five minutes
+        // There are a lot of discussions about this on StackOverflow. The only functional solution
+        // is to p/invoke to DeleteUrlCacheEntry
+        static readonly System.Net.Cache.HttpRequestCachePolicy s_cachePolicy =
+            new System.Net.Cache.HttpRequestCachePolicy(System.Net.Cache.HttpRequestCacheLevel.BypassCache);
+
+        static void ReadData(string url, DataSet dataSet, DataType dataType)
+        {
+            HttpWebRequest.DefaultCachePolicy = s_cachePolicy;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.CachePolicy = s_cachePolicy;
+            request.Headers.Set("Cache-Control", "max-age=0, no-cache, no-store");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Expected Header: Province/State,Country/Region,Lat,Long,1/22/20,other dates
+            using (var reader = new CsvReader(new StreamReader(response.GetResponseStream(), Encoding.UTF8, true), true))
+            {
+                // Read the header line
+                var header = reader.Read();
+
+                // Ensure the format is what we expect
+                if (header[0] != "Province/State"
+                    || header[1] != "Country/Region"
+                    || header[2] != "Lat"
+                    || header[3] != "Long"
+                )
+                {
+                    throw new Exception("Unexpected data header format.");
+                }
+
+                // Read the dates
+                var dates = new List<DateTime>();
+                for (int i=4; i<header.Length; ++i)
+                {
+                    dates.Add(ParseSimpleDate(header[i]));
+                }
+
+                // Read the data
+                for (; ; )
+                {
+                    var line = reader.Read();
+                    if (line == null) break;
+
+                    if (line.Length != dates.Count + 4)
+                    {
+                        throw new Exception($"Unexpected input data. Expected {dates.Count} entries in the date series. Found {line.Length - 4}.");
+                    }
+
+                    for (int i=0; i<dates.Count; ++i)
+                    {
+                        AddData(dataSet, dates[i], line[0], line[1], line[2], line[3], int.Parse(line[i + 4]), dataType);
+                    }
+                }
+            }
+        }
+
+        static void WriteData(DataSet dataSet, string path)
+        {
+            using (var writer = new StreamWriter(path, false, s_UTF8_No_BOM))
+            {
+                writer.NewLine = "\n";
+                writer.WriteLine("\"Date\",\"ProvinceState\",\"CountryRegion\",\"Lat\",\"Long\",\"Confirmed\",\"Deaths\",\"Recovered\"");
+
+                var dateList = new List<KeyValuePair<DateTime, Dictionary<DataKey, DataRecord>>>(dataSet);
+                dateList.Sort((a, b) => a.Key.CompareTo(b.Key));
+                foreach(var datePair in dateList)
+                {
+                    var recordList = new List<KeyValuePair<DataKey, DataRecord>>(datePair.Value);
+                    recordList.Sort((a, b) => a.Key.CompareTo(b.Key));
+                    foreach(var recordPair in recordList)
+                    {
+                        writer.WriteLine(ToString(recordPair));
+                    }
+                }
+            }
+        }
+
+        private static DateTime ParseSimpleDate(string strDate)
+        {
+            string[] parts = strDate.Split('/');
+            if (parts.Length != 3) throw new Exception($"Unexpected date format: '{strDate}'");
+            int year = int.Parse(parts[2]) + 2000;
+            int month = int.Parse(parts[0]);
+            int day = int.Parse(parts[1]);
+
+            var date = new DateTime(year, month, day, 0, 0, 0, DateTimeKind.Unspecified);
+            //Console.WriteLine(date);
+            return date;
+        }
+
+        private static void AddData(DataSet dataset, DateTime date, string provinceState, string countryRegion, string latitude, string longitude, int data, DataType dataType)
+        {
+            Dictionary<DataKey, DataRecord> dateDict;
+            if (!dataset.TryGetValue(date, out dateDict))
+            {
+                dateDict = new Dictionary<DataKey, DataRecord>();
+                dataset.Add(date, dateDict);
+            }
+
+            var dataKey = new DataKey(date, provinceState, countryRegion, latitude, longitude);
+
+            DataRecord dataRecord;
+            if (!dateDict.TryGetValue(dataKey, out dataRecord))
+            {
+                dataRecord = new DataRecord();
+                dateDict.Add(dataKey, dataRecord);
+            }
+
+            switch (dataType)
+            {
+                case DataType.Confirmed:
+                    dataRecord.Confirmed = data;
+                    break;
+
+                case DataType.Deaths:
+                    dataRecord.Deaths = data;
+                    break;
+
+                case DataType.Recovered:
+                    dataRecord.Recovered = data;
+                    break;
+
+                default:
+                    Debug.Fail("Unexpected DataType");
+                    break;
+            }
+        }
+
+        static string ToString(KeyValuePair<DataKey, DataRecord> pair)
+        {
+            return String.Join(",",
+                pair.Key.Date.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture),
+                string.Concat("\"", pair.Key.ProvinceState, "\""),
+                string.Concat("\"", pair.Key.CountryRegion, "\""),
+                pair.Key.Latitude,
+                pair.Key.Longitude,
+                pair.Value.Confirmed,
+                pair.Value.Deaths,
+                pair.Value.Recovered);
+        }
+
+    } // Class Program
+
+    class DataKey : IComparable<DataKey>
+    {
+        public DateTime Date { get; private set; }
+        public string ProvinceState { get; private set; }
+        public string CountryRegion { get; private set; }
+        public string Latitude { get; private set; }
+        public string Longitude { get; private set; }
+
+        public DataKey(DateTime datex, string provinceState, string countryRegion, string latitude, string longitude)
+        {
+            Date = datex;
+            ProvinceState = provinceState;
+            CountryRegion = countryRegion;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public override int GetHashCode()
+        {
+            return Date.GetHashCode()
+                ^ ProvinceState.GetHashCode()
+                ^ CountryRegion.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as DataKey;
+            if (obj == null) return false;
+
+            return Date.Equals(other.Date)
+                && ProvinceState.Equals(other.ProvinceState)
+                && CountryRegion.Equals(other.CountryRegion);
+        }
+
+        public int CompareTo(DataKey other)
+        {
+            int i = Date.CompareTo(other.Date);
+            if (i != 0) return i;
+            i = CountryRegion.CompareTo(other.CountryRegion);
+            if (i != 0) return i;
+            return ProvinceState.CompareTo(other.ProvinceState);
+        }
+    }
+
+    class DataRecord
+    {
+        public int Confirmed;
+        public int Deaths;
+        public int Recovered;
+    }
+}
